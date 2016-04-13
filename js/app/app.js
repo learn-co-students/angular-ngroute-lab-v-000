999a8e75d74faf8848db657b4893add9eb8677f0
Adam King <adam.king0126@gmail.com>
2016-04-13 12:26:15 -0500

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -2,12 +2,12 @@ angular
     .module('app', ['ngRoute'])
     .config(function ($routeProvider) {
     $routeProvider
-      .when('/user/:name', {
+      .when('/user/:id', {
         templateUrl: 'views/user.html',
         controller: 'UserController as user',
         resolve: {
           user: function ($http, $route) {
-            return $http.get('http://0.0.0.0:8882/rest/user/' + $route.current.params.name);
+            return $http.get('http://0.0.0.0:8882/rest/user/' + $route.current.params.id);
           }
         }
       });
