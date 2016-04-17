0009c20e0a1d6a7e204533a7c857a571868b7a4c
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-04-17 11:47:23 -0400

    Done

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,16 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ngRoute'])
+    .config(function($routeProvider){
+
+      $routeProvider
+        .when('/user/:id', {
+          templateUrl: 'views/user.html',
+          controller: 'UserController as user',
+          resolve: {
+            user: function($route, UserService){
+              return UserService.getUser($route.current.params.id);
+            }
+          }
+        });
+
+    });
