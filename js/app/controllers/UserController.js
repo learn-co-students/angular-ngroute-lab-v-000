4a7b013bd8e30afeafd6aa964ad238b7dca5956b
Marco Tsui <marcotsui2003@yahoo.com>
2016-05-05 11:53:23 -0400

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -2,5 +2,17 @@ function UserController() {
 }
 
 angular
-	.module('app')
-	.controller('UserController', UserController);
+	.module('app',['ngRoute'])
+	.controller('UserController', UserController)
+	.config(function($routeProvider) {
+		$routeProvider
+		.when('/user/:id', {
+			templateUrl: 'views/user.html',
+			controller: 'UserController',
+			resolve: {
+				user: function($routeParams, UserService) {
+				return UserService.getUser($routeParams.name);
+			  }
+			}
+		})
+	});
