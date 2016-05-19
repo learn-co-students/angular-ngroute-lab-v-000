37d6ac3b23e270a41e62ba058f07bcda057527ac
Louiefigz <figueroa.aaron@gmail.com>
2016-05-19 14:41:44 -0400

    Done.

--- js/app/services/UserService.js
+++
@@ -1,9 +0,0 @@
-function UserService ($http) {
-  this.getUser = function (name) {
-    return $http.get('/user/' + name);
-  }
-}
-
-angular
-  .module('app')
-  .service('UserService', UserService);
