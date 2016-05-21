2a030dd586c0e7b0dcc4d71b53c39206e56c8a4a
c1505 <coreym247@gmail.com>
2016-05-20 21:52:26 -0400

    work in progress

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,13 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ngRoute'])
+    .config(function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          template: 'animal is {{first}}',
+          controller: 'UserController'
+        })
+        .when('/user/:id', {
+          templateUrl: 'views/user.html',
+          controller: 'UserController'
+        });
+    });
--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,4 +1,13 @@
-function UserController() {
+function UserController($scope, $route, $routeParams, $http) {
+  $scope.getUser = function (id) {
+    $http.get('http://0.0.0.0:8882/rest/user/' + id)
+    .then(function (res) {
+      $scope.first = res.data.name.first;
+      $scope.last = res.data.name.last;
+      $scope.email = res.data.email;
+    });
+  };
+  $scope.getUser($routeParams.id);
 }
 
 angular
