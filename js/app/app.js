e23155b3435a9b9c74abd12bc57690950e9987b0
shinyrachel <rachel.kory@gmail.com>
2016-06-06 10:15:55 -0400

    complete

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,14 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ngRoute'])
+    .config(function ($routeProvider) {
+      $routeProvider
+        .when('/user/:id', {
+          templateURL: '/views/user.html',
+          controller: 'UserController as ctrl',
+          resolve: {
+            user: function ($routeParams, $http) {
+              return $http.get('http://0.0.0.0:8882/rest/user/' + $routeParams.id);
+            }
+          }
+        });
+    });
