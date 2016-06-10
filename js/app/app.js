bc44dd6be997243c30742d7b3380f7983c40dfeb
viparthasarathy <viparthasarathy@gmail.com>
2016-06-10 13:50:47 -0700

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,17 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ngRoute'])
+    .config(function ($routeProvider) {
+      $routeProvider
+        .when('/user/:id', {
+          templateUrl: 'views/user.html',
+          controller: 'UserController as ctrl',
+          resolve: {
+            user: function($http, $routeParams) {
+              $http.get('http://0.0.0.0:8882/rest/user/' + $routeParams.id)
+              .then(function(res) {
+                ctrl.user = res.data;
+              });
+            }
+          }
+        });
+    });
