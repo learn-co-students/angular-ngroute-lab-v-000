206091366e8276676ce90402affcc2c8620a3931
Tyler Smith <oats000@hotmail.com>
2016-06-30 22:36:12 +0000

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,6 +1,20 @@
-function UserController() {
+function UserService($http) {
+    this.getUser = function (name) {
+        return $http.get('http://0.0.0.0:8882/rest/user/' + namr);
+    };
+}
+
+function UserController($routeParams, UserService) {
+    var ctrl = this;
+
+    UserService
+        .getUser($routeParams.name)
+        .then(function (res) {
+            ctrl.user = res.data; // our user object is populated from the backend
+        });
 }
 
 angular
 	.module('app')
-	.controller('UserController', UserController);
+	.controller('UserController', UserController)
+	.service('UserService', UserService);
