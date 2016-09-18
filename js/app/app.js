ec05d1179c9053665c0f65f4e5087031b2468be6
hudbuz <hzb2@cornell.edu>
2016-09-17 22:39:51 -0400

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,23 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ngRoute'])
+    .config(function($routeProvider){
+  
+      $routeProvider
+        .when('/user/:id', {
+           
+          templateUrl: 'views/user.html',
+          controller: 'UserController as user',
+          resolve: {
+            
+            user: function($route, $http){
+          
+              var resp =  $http.get('http://0.0.0.0:8882/rest/user/'+$route.current.params.id)
+              debugger
+              return resp
+            }
+          }
+        }
+
+
+        )
+    })
---
+++ js/app/services/UserService.js
@@ -0,0 +1,11 @@
+// function UserService($http){
+//   debugger
+//   this.getUser = function(name){
+//     debugger
+//     return $http.get('http://0.0.0.0:8080/rest/user/'+id)
+//   }
+// }
+
+// angular 
+//   .module('app')
+//   .service('UserSerice', UserService)
