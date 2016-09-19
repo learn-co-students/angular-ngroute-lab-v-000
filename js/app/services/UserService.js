fcfb4e0262da1bebee50234ad669d3a50ca5a140
cassaram09 <cassaram09@gmail.com>
2016-09-19 17:23:33 -0400

    Done.

--- js/app/services/UserService.js
+++
@@ -1,9 +0,0 @@
-function UserService($http){
-  this.getUser = function(name){
-    return $http.get('http://0.0.0.0:8882/rest/user/' + name);
-  };
-}
-
-angular
-  .module('app')
-  .service('UserService', UserService);
