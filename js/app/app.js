4c623c32481111cbd2dce4be5a74d5aba037086d
aprietof <adrianprieto00@gmail.com>
2016-09-28 19:05:05 -0400

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,17 @@
 angular
-    .module('app', ['']);
+  .module('app', ['ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/user/:id', {
+        // Configuration Object
+        // View Configuration
+        templateUrl: 'views/user.html',
+        // Controller Configuration
+        controller: 'UserController as user',
+        resolve: {
+          user: function ($http, $route) {
+            return $http.get('http://0.0.0.0:8882/rest/user/' + $route.current.params.id)
+          }
+        }
+      });
+  });
