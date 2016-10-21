02f071baf1b75257729df685e6a3ff43fd2cade3
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-10-21 06:25:48 -0400

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,15 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ngRoute'])
+    .config(function($routeProvider) {
+      $routeProvider
+        .when('/user/:id', {
+          templateUrl: 'views/user.html',
+          controller: 'UserController',
+          resolve: {
+                    user: function ($routeParams) {
+                        // return $http.get('/user/'+$routeParams.name);
+                      return UserService.getUser($routeParams.id);
+                    }
+                }
+        });
+    });
