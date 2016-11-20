200eb882a3129030284a96e20fe11aec2ef38412
KerryAlsace <kerrydougherty@gmail.com>
2016-11-20 14:09:57 -0500

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -2,7 +2,7 @@ angular
     .module('app', ['ngRoute'])
       .config(function($routeProvider) {
         $routeProvider
-          .when('/user/:name', {
+          .when('/user/:id', {
             templateUrl: 'views/user.html',
             controller: 'UserController as ctrl',
             resolve: {
