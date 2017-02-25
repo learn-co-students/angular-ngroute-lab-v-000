ee278d127e0cf8018af1bcacac5c18a394ff9d26
tttra <tothutra@gmail.com>
2017-02-25 09:56:56 -0500

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,14 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ngRoute'])
+    .config(function ($routeProvider) {
+      $routeProvider
+        .when('/user/:id', {
+          templateUrl: 'views/user.html',
+          controller: 'UserController as ctrl',
+          resolve: {
+            user: function ($http, $routeParams) {
+              return $http.get("http://0.0.0.0:8882/rest/user/" + $routeParams.id)
+            }
+          }
+        })
+    })
