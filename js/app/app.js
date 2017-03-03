18757a6dfe194bf96d427292042fb8469b6a4132
BrunoBoehm <bruno.boehm@gmail.com>
2017-03-03 16:00:13 +0100

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,15 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ngRoute'])
+    .config(function($routeProvider){
+    	$routeProvider
+    		.when('/user/:name', {
+    			templateUrl: 'views/user.html',
+    			controller: 'UserController as ctrl',
+    			resolve: {
+    				user: function ($route, $http) {
+    					console.log($route);
+    					return $http.get("http://0.0.0.0:8882/rest/user/" + $route.current.params.name);
+    				}
+    			}
+    		})
+    });
